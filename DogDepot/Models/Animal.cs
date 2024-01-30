7832d7c687a731eb882bba5fff5a0b5c202923a5
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-30 10:42:54 -0800

    controller view work
    Co-Authored-By: Ravin Fisher <ravesterrr@gmail.com>
    
    Co-Authored-By: Gabriel Tucker <gabrieltucker260@gmail.com>

--- DogDepot/Models/Animal.cs
+++ DogDepot/Models/Animal.cs
@@ -5,17 +5,9 @@ namespace DogDepot.Models
     public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Breed { get; set; }
-    public DateTime Date { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
     public int Age { get; set; }
     public string Description { get; set; }
 
-    public Animal(string name, string breed, DateTime date, int age, string description)
-    {
-      Name = name;
-      Breed = breed;
-      Date = date;
-      Age = age;
-      Description = description;
-    }
   }
 }
